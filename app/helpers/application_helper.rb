0bec8382cc4a3ddfbdbbc53b49bbd53a7f0e93c0
John Perkins <jeperkins4@gmail.com>
2016-05-12 17:02:57 -0400

    Fix to media

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def project_image_link(document)
-    content_tag(:img, nil, src: document.media.download_url(document.media), width: '700px', height: '300px', alt: document.name, class: 'img-responsive') unless document.media.blank?
+    content_tag(:img, nil, src: document.media.download_url(document.media), width: '700px', height: '300px', alt: document.name, class: 'img-responsive') unless document.try(:media).blank?
   end
 
   def resource_name
--- app/models/document.rb
+++ app/models/document.rb
@@ -1,4 +1,6 @@
 class Document < ActiveRecord::Base
   belongs_to :project
   mount_uploader :media, DocumentUploader
+
+  validate :media, presence: true
 end
